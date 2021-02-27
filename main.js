eb76292fdba378d4b4f96a8cec81b25712126ac9
Roman <68850770+Roman27011986@users.noreply.github.com>
2021-02-27 22:43:34 +0200

    йу

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ rootRef.addEventListener('click', event => {
         rootItemRef.style.transform=`translate(${trX}px,${trY}px)` 
    
 })
-
+console.dir(root.style.transform)
 const onEntry = ((entries, observer) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -20,7 +20,7 @@ const onEntry = ((entries, observer) => {
 const options = {
     root:rootItemRef,
      rootMargin: '10px',
-    // threshold: 1.0
+     threshold: 1.0
 } 
 const io = new IntersectionObserver(onEntry, options) 
 io.observe(root)
