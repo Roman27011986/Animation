6e16fb4494eb4529b9137f7539dfc04e9ca0b16a
Roman <68850770+Roman27011986@users.noreply.github.com>
2021-02-27 21:52:10 +0200

    ц

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
 const rootRef = document.querySelector('.root')
 const rootItemRef = document.querySelector('.root__item')
+const root= document.querySelector(".root__item--target")
+
 rootRef.addEventListener('click', event => {
     const trY = event.layerY
     const trX = event.layerX
         rootItemRef.style.transform=`translate(${trX}px,${trY}px)` 
-    console.log(event)
+   
 })
+
+const onEntry = ((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+           
+            console.log(entry)
+        }
+    })
+})
+const options = {
+    root:rootItemRef,
+     rootMargin: '10px',
+    // threshold: 1.0
+} 
+const io = new IntersectionObserver(onEntry, options) 
+io.observe(root)
+
+
+    
+    
+
+
+
